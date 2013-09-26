d46d328ca4fbd2398b0f282172591ae01edfde4b
Clay Stewart <claytonmstewart@gmail.com>
2013-09-26 10:12:58 -0500

    Add video show by id method to controller and view

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -5,13 +5,7 @@ class VideosController < ApplicationController
 	end
 
 	def show
-		videos = {
-			"gladiator" =>
-				{title: "Gladiator 'Are You Not Entertained' Scene", youtube_id: "FI1ylg4GKv8"}, 
-			"newsroom"=>
-				{title: "America is not the best", youtube_id: "q49NOyJ8fNA"}
-		}
-		@video = videos[params[:video_name]]
+		@video = Video.find(params[:id])
 	end
 
 end
